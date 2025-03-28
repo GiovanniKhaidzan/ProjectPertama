41dfddccf647128c0391d1fa95f2b8d840fbcc4d
Griffinnniyoo <giovannikhaidzan@gmail.com>
2025-03-28 00:51:46 -0700

    Menghapus .git dalam client dan menambahkan file

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+import LandingPage from './pages/LandingPage'
+
+function App() {
+
+
+  return (
+    <LandingPage/>
+  )
+}
+
+export default App
